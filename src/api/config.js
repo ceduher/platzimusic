2bc24af3839049c9c8d6a028c3abdf81580c15de
Cesar Hernandez <niumared@gmail.com>
2020-09-14 12:16:36 -0400

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default{
+    appName: 'platzimusic',
+    apiKey: '49281a0a88365c55edf2810192cf48e2',
+    secret: '206a6fd3472bbfa7feb2266d1bc0753a',
+    registeredTo: 'ceduher'
+}
+
+//Application name	platzimusic
+//API key	        49281a0a88365c55edf2810192cf48e2
+//Shared secret	    206a6fd3472bbfa7feb2266d1bc0753a
+//Registered to	    ceduher
